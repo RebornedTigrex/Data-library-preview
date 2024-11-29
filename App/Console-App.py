2c9f84b5eff4a090d61cf06cac30a8c669274ded
RebornedTigrex <157234124+RebornedTigrex@users.noreply.github.com>
2024-11-29 04:00:27 +0300

    Почти всё готово.

--- App/Console-App.py
+++ App/Console-App.py
@@ -1,15 +1,75 @@
 #from App.Core1 import BookAction
 from Core import BookAction
+import time, keyboard
+
 
 manager = BookAction()
 
-manager.add("Да", "Тодасё", 1866)
-manager.add("1984", "Тод Говард", 1949)
+class m:
+    def dM():
+        print("Книга удалена!")
+        return ""
+
+    def pressBtn():
+        print("\n Для продолжения нажмите любую клавишу:\n")
+        while keyboard.read_key() == False:
+            pass
+        return ""
+
+def auto():
+    print("Приготовьтесь, мы добавляем книгу:")
+    time.sleep(1)
+    print("Автор этой книги будет: Пьер Кюри,")
+    time.sleep(0.5)
+    print("Название будет: 'Игра радиоактивным стержнем на рояле'")
+    time.sleep(0.5)
+    print("Год будет: 1685\n")
+    
+    manager.add("Игра радиоактивным стержнем на рояле","Пьер Кюри",1685)
+    time.sleep(1)
+    print("Добавим ещё парочку книг для наглядности. \nId ключ будет создан по порядку: Если какой-то элемент будет удалён - его id будет занято новым объектом.")
+    
+    manager.add("Увеселительные напитки: Бытие бармена", "Тодасё", 1866)
+    manager.add("1984", "Тод Говард", 1949, count=2)
+
+    print("Сейчас вы можете взглянуть в Data/books.json.",m.pressBtn())
+
+    print("А сейчас мы удалим книгу Тода Говарда из базы данных.")
+    time.sleep(1)
+    print(" Вот дела! Я не знаю id этой книги! Придётся найти её с помощью метода search.")
+    time.sleep(0.5)
+    temp = manager.search("Тод Говард","author")[0][1]
+    print(f"Id книги Говарда будет:{temp}. Удалим же её.")
+    time.sleep(0.5)
+    m.dM()
+    print("Теперь вы можете взглянуть снова на наш файл.", m.pressBtn())
+
+    print("Мы можем забрать книгу из нашей библиотеки: Для этого есть метод take")
+    
+    
+    
+
+
+if __name__ == "__main__":
+    print("Здравствуйте! Вы запустили СУБД для управления книгами.")
+    while i.lower() not in ["autotest", "selftest", "a", "s"]:
+        i = input("Если вы хотите запустить тест: напишите autoTest (a),\n если вы хотите попробовать функционал самостоятельно, то напишите selfTest (s)")
+
+    match i.lower()[:1]:
+        case "a":
+            auto()
+        
+
+
+
+# manager = BookAction()
+
+# manager.add("Да", "Тодасё", 1866)
+# manager.add("1984", "Тод Говард", 1949, count=1)
 
 # manager.remove("0")
+# print(manager.take("1"))
+# manager.add("Да", "Тодасё", 1866)
 
-print("Информация о книге '1984':", manager.getInfo("1984"))
-# print("Список всех книг:", manager.allBooks())
-
-# manager.remove(1)
-# print("Список всех книг после удаления:", manager.allBooks())
+# print("Информация о книге '1984':", manager.search("1984", "title"))
+# print("Список всех книг:", manager.allPositions())
--- App/Core.py
+++ App/Core.py
@@ -17,74 +17,104 @@ class BookAction:
         self.file_path = file_path
         if not os.path.exists(self.file_path):
             with open(self.file_path, mode="w+", encoding="utf-8") as file:
-                json.dump({"Books": {}},file)
-    # TODO add
-    def add(self, title:str, author:str, year:str, count = 1) -> None:
+                json.dump({"Books": {}},file, ensure_ascii=False)
+        with open(DB_path, "r+", encoding="utf-8") as file:
+            try:
+                json.load(file)
+            except:
+                print("[JSONDecodeError] in methood __init__: Файл базы данных не соответствует структуре Json. Удалите файл или исправьте вручную.")
+    
+    def add(self, title:str, author:str, year:int|str, count = 1) -> None:
         """Добавление книги в базу данных/ добавление к существующей книге параметра count. \n
         :param title: Название книги
         :param author: Автор книги
         :param year: Год написания книги"""
-        # try:
-        #     # Проверка типов переменных
-        #     if(not isinstance(author, str) and not isinstance(title, str)):
-        #         raise Exception(TypeError, "Author и title должен быть string!")
+
+        try:
+            # Проверка типов переменных
+            if(not isinstance(author, str) and not isinstance(title, str)):
+                raise Exception("[TypeError] Author и title должен быть string!")
+            if(count <= 0):
+                raise Exception("[RealityError] Ошибка. Число книг должно быть вещественным целым.")
         # ____________________________________________________________
-        statusHave = "На хранении"
-        with open(DB_path, "r+", encoding="utf-8") as file:
-            data = json.load(file)
-            subObj = data["Books"]
-            for obj in subObj.keys():
-                if subObj[obj]["title"].lower() == title.lower():
-                    subObj[obj]["count"] += 1
-                    subObj[obj]["status"] = statusHave
+            statusHave = "На хранении"
+            with open(DB_path, "r+", encoding="utf-8") as file:
+                data = json.load(file)
+                subObj = data["Books"]
+                for obj in subObj.keys():
+                    if subObj[obj]["title"] == title:
+                        subObj[obj]["count"] += count
+                        subObj[obj]["status"] = statusHave
+                        
+                        data["Books"] = subObj
 
-            
-            next_id = len(data["Books"].keys())
-            data["Books"].update({next_id: {"title": title, "author": author, "year": year, "count": int(count), "status": statusHave}})
+                        file.seek(0)
+                        json.dump(data, file, indent=4, ensure_ascii=False)
+                        return(None)
 
-            file.seek(0)
-            json.dump(data, file, indent=4)
-        
+                next_id = 0
+
+                while True:
+                    if str(next_id) not in data["Books"].keys():
+                        break
+                    next_id += 1
+                
+                data["Books"].update({str(next_id): 
+                                      {"title": title,
+                                        "author": author,
+                                        "year": str(year), 
+                                        "count": int(count),
+                                        "status": statusHave}})
+
+                file.seek(0)
+                json.dump(data, file, indent=4, ensure_ascii=False)
+                return(None)
         # ---------------------------------------------------
-        # except Exception as err:
-        #     print(err)
-        #     return(-1)
+        except Exception as err:
+            print("Panic! In methood add:",err)
+        finally:
+            file.close()
     
-    def search(self, identificator:str, det = "id" """, pointerOut = False""") -> list[dict]|None:
-        """:param det: На выбор предоставляется поиск по title, id, author, year \n"""
+    def search(self, identificator:str, det:str) -> list[tuple[dict, str]]|None:
+        """
+        Функция поиска по любому идентификатору книги. Возвращает ```list[tuple[dict, str]]```, где ```dict``` это объект книга, а ```str``` это ```id``` этой книги в базе данных. \n
+        :param identificator: Введите название книги, либо ```id```, либо автора, либо год написания.
+        :param det: На выбор предоставляется поиск по title, id, author, year
+        """
         # :param pointerOut: Позволяет получить все номера битов объектов подходящих по заданным условиям
         try:
+            if det not in ["title", "id", "author", "year"]:
+                raise Exception("[DetError] Вы указали неверный идентификатор данных! Используйте: title, id, author или year")
+            
             with open(self.file_path, mode="r",encoding="utf-8") as file:
                 out = []
+                identificator = str(identificator).lower()
                 data = json.load(file)
+                subObj = data["Books"]
                 match det:
                     case "title":
-                        for obj in data["Books"].keys():
-                            if data[obj]["title"] == identificator:
-                                out.append(data[obj])
+                        for obj in subObj.keys():
+                            if subObj[obj]["title"].lower() == identificator:
+                                out.append([subObj[obj], obj])
                     case "id":
-                        for obj in data["Books"].keys():
+                        for obj in subObj.keys():
                             if obj == identificator:
-                                return data[obj]
+                                return [subObj[obj], obj]
                     case "author":
-                        for obj in data["Books"].keys():
-                            if data[obj]["author"] == identificator:
-                                out.append(data[obj])
+                        for obj in subObj.keys():
+                            if subObj[obj]["author"].lower() == identificator:
+                                out.append([subObj[obj], obj])
                     case "year":
-                        for obj in data["Books"].keys():
-                            if data[obj]["year"] == identificator:
-                                out.append(data[obj])
+                        for obj in subObj.keys():
+                            if str(subObj[obj]["year"]) == str(identificator):
+                                out.append([subObj[obj], obj])
                 if len(out) == 0:
-                     raise Exception(NameError)
+                        raise Exception("[DataNotExist] Такого идентификатора не существует в базе данных!")
                 return(out)
         except Exception as err:
-            print(err, "Такого идентификатора не существует в базе данных!")
-            return(None)
-        
-        #     raise Exception(NameError)
-        # except Exception as er:
-        #     print(er, "Такого идентификатора не существует в базе данных!")
-        #     return None
+            print("Panic! In methood search:",err)
+        finally:
+            file.close()
         
     
     # def getInfo(self, title):
@@ -103,45 +133,57 @@ class BookAction:
     def take(self, id:str, count = 1, ForseStatus = 0) -> list[dict]:
         """Функция изменения статуса выдачи книги. Будут затронуты поля status и count.\n
         :param id: Этот параметр принимает id номер книги.
-        :param count: Необязательный параметр. По умолчанию этот параметр равен ```1```. Он отвечает за количество взятых книг из базы данных
-        :param ForseStatus: Необязательный параметр. По умолчанию этот параметр равен ```0```. Он насильно присваевает значение ```status``` в таблице. Если значение ```1```, метод игнорирует количество книг и насильно присваевает значение ```Нет в наличии```. Если значение ```2```, метод игнорирует автоматику и присваевает ```Есть в наличии```.
+        :param count: Необязательный параметр. По умолчанию этот параметр равен ```1```. Параметр отвечает за количество взятых книг из базы данных
+        :param ForseStatus: Необязательный параметр. По умолчанию этот параметр равен ```0```. Параметр насильно присваевает значение ```status``` в таблице. Если значение ```1```, метод игнорирует количество книг и насильно присваевает значение ```Нет в наличии```. Если значение ```2```, метод игнорирует автоматику и присваевает ```Есть в наличии```.
         """
+        try:
+            with open(self.file_path, mode="r+",encoding="utf-8") as file:
+                data = json.load(file)
+                for obj in data["Books"].keys():
+                    if obj == id:
+                        subObj = data["Books"][obj]
+                        if subObj["count"] - count >= 0:
+                            subObj["count"] -= count
+                            if subObj["count"] <= 0 and ForseStatus == 0:
+                                subObj["status"] = "Нет в наличии"
+                            elif ForseStatus <= 1:
+                                subObj["status"] = "Нет в наличии"
+                            elif ForseStatus >= 2:
+                                subObj["status"] = "На хранении"
+                            file.seek(0)
+                            data["Books"][obj] = subObj
+                            json.dump(data, file, indent=4, ensure_ascii=False)
+                            return subObj
+                        else:
+                            raise Exception(f"[CountError] Количество этих книг: {data["Books"][obj]["count"]}. Невозможно взять книгу!")
+        except Exception as err:
+            print("Panic! In methood take:",err)
+        finally:
+            file.close()
 
-        with open(self.file_path, mode="w+",encoding="utf-8") as file:
-            data = json.load(file)
-            for obj in data["Books"].keys():
-                if obj == id:
-                    subObj = data["Books"][obj]
-                    if subObj["count"] - count >= 0:
-                        subObj["count"] -= count
-                        if subObj["count"] <= 0 and ForseStatus == 0:
-                            subObj["status"] = "Нет в наличии"
-                        elif ForseStatus <= 1:
-                            subObj["status"] = "Нет в наличии"
-                        elif ForseStatus >= 2:
-                            subObj["status"] = "На хранении"
-                        return subObj
-                    else:
-                        raise Exception(f"Количество книг {data["Books"][obj]["count"]}. Невозможно взять новую!")
 
- 
+    def remove(self, item:str)-> None:
+        """Функция полностью удаляет запись из базы данных
+        :param item: В этот параметр передаётся """
 
-    def remove(self, id:str)-> None:
-        """Функция полностью удаляет запись из базы данных"""
-
-        with open(self.file_path, mode="r+",encoding="utf-8") as file:
-            data = json.load(file)
-        del data["Books"][id]
+        Exception = "Такого id не существует!"
+        try:
+            with open(self.file_path, mode="r",encoding="utf-8") as file:
+                data = json.load(file)
+            del data["Books"][item]
+                
+            with open(self.file_path, mode="w",encoding="utf-8") as file:
+                file.seek(0)
+                json.dump(data, file, indent=4, ensure_ascii=False)
             
-        with open(self.file_path, mode="w",encoding="utf-8") as file:
-            file.seek(0)
-            json.dump(data, file, indent=4)
-    
+        except Exception as err:
+            print("Panic! In methood remove:",err)
 
     def allPositions(self)-> list[dict]:
         out = []
         with open(self.file_path, mode="r+",encoding="utf-8") as file:
             data = json.load(file)
-            for item in data["Books"]:
-                out.append(item)
-        return(out)
+            for item in data["Books"].keys():
+                out.append(data["Books"][item])
+            return(out)
+        # return(data["Books"])
